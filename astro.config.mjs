511279c562d5e5bd3e82ea24f13bf4f414e3a5f8
Reli <naitvenefy@gmail.com>
2026-01-18 21:38:26 -0600

    setup for vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
 import svelte from "@astrojs/svelte";
+import vercel from "@astrojs/vercel/static";
 
-// https://astro.build/config
 export default defineConfig({
+  output: "static",
+  adapter: vercel({}),
   vite: {
     plugins: [tailwindcss()],
   },
-
   integrations: [svelte()],
 });
---
+++ astro.config.mjs.temp.vercel.mjs
@@ -0,0 +1,3 @@
+import vercel from "@astrojs/vercel/static";
+
+export default vercel();
